658618b1ac40e7cadfb3292b5660713a3724d271
EDDYtu22 <201221004@fdiba.tu-sofia.bg>
2023-03-19 10:37:24 +0200

    test Connection to h2 Database

--- project1/src/main/java/dev/edmond/App.java
+++ project1/src/main/java/dev/edmond/App.java
@@ -1,4 +1,10 @@
 package dev.edmond;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 /**
  * Hello world!
@@ -8,6 +14,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            Connection conn = DriverManager
+            .getConnection( "jdbc:h2:~/students", "edmond", "123456");
+
+            System.out.println("Connection valid/no: " + conn.isValid(5));
+
+            String querySelectAll = "SELECT * FROM Students";
+            Statement statementSelectAll  = conn.createStatement();
+            ResultSet rs = statementSelectAll.executeQuery(querySelectAll);
+
+            while(rs.next()){
+                System.out.print(rs.getString("ID") + " ");
+                System.out.print(rs.getString("FirstName") + " ");
+                System.out.print(rs.getString("LastName"));
+                System.out.println();
+            }
+
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
---
+++ project1/src/main/java/dev/edmond/DB/DBConnectior.java
@@ -0,0 +1,12 @@
+package dev.edmond.DB;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+public class DBConnectior {
+    
+    public DBConnectior(){}
+
+   
+}
