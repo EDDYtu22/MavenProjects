12ea94bc0225436c48a94d1921e65c6f0351faa5
EDDYtu22 <201221004@fdiba.tu-sofia.bg>
2023-03-20 14:30:42 +0200

    feat: mysql db server connection

--- project1/src/main/java/dev/edmond/App.java
+++ project1/src/main/java/dev/edmond/App.java
@@ -1,33 +1,33 @@
 package dev.edmond;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
         try {
             Connection conn = DriverManager
-            .getConnection( "jdbc:h2:~/students", "edmond", "123456");
+                    .getConnection("jdbc:mysql://localhost:3306/sql_hr", "root", "Oskar2015");
 
             System.out.println("Connection valid/no: " + conn.isValid(5));
 
-            String querySelectAll = "SELECT * FROM Students";
-            Statement statementSelectAll  = conn.createStatement();
-            ResultSet rs = statementSelectAll.executeQuery(querySelectAll);
+            String querySelectAll = "SELECT * FROM sql_hr.employees";
+            Statement statementSelectAll = conn.createStatement();
+            ResultSet rs =  statementSelectAll.executeQuery(querySelectAll);
 
-            while(rs.next()){
-                System.out.print(rs.getString("ID") + " ");
-                System.out.print(rs.getString("FirstName") + " ");
-                System.out.print(rs.getString("LastName"));
+            while (rs.next()) {
+                System.out.print(rs.getString("employee_id") + "  ");
+                System.out.print(rs.getString("first_name") + " ");
+                System.out.print(rs.getString("last_name"));
+                System.out.print(org.apache.commons.lang3.StringUtils.repeat(' ',
+                        20 - (rs.getString("first_name").length() + rs.getString("last_name").length())));
+                System.out.print(rs.getString("job_title"));
                 System.out.println();
             }
 
